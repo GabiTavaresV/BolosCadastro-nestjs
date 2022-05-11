18d7a786c1f1aec98ef3e5c144bb5d8e2531f12d
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-05-11 19:34:27 -0300

    ✔️feat: add buscaPorBoloById

--- src/bolos/bolos.service.ts
+++ src/bolos/bolos.service.ts
@@ -4,11 +4,18 @@ import { Bolos } from './bolos.entity';
 
 @Injectable()
 export class BolosService {
+  findAll() {
+    throw new Error('Method not implemented.');
+  }
   private bolos: Array<Bolos> = [];
 
   public cadatraBolos(bolo: Bolos): Bolos {
     this.bolos.push(bolo);
 
     return bolo;
+  }
+
+  public buscaPorBoloById(id: number): Bolos {
+    return this.bolos.find((bolo) => bolo.id === id);
   }
 }
